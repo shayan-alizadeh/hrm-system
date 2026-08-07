5da05f3d340f0cf3e0010535c7d09b26632123a4
shyn <shynaz87@gmail.com>
2026-08-07 06:43:02 -0700

    configuration .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const appName = configService.get('APP_NAME');
+  const port = configService.get('PORT', 3001);
+  await app.listen(port);
+  console.log(`Application ${appName} is running on port : ${port}`);
 }
 bootstrap();
