d40506566cea155f16b0954d8619f9e366c0952c
shyn <shynaz87@gmail.com>
2026-08-08 05:55:35 -0700

    feat : complete departments-manager.controllers.ts

---
+++ src/modules/departments/controllers/departmants-manager.controller.ts
@@ -0,0 +1,66 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { DepartmentsManagerService } from '../services/departments-manager.service';
+import { CreateDepartmentDto } from '../dto/create-department.dto';
+import { UpdateDepartmentDto } from '../dto/update-department.dto';
+// import { Department } from '../entities/department.entity';
+// import { ApiBearerAuth } from '@nestjs/swagger';
+// import { Role } from '../../../shared/enums/user-role.enum';
+// import { Roles } from '../../../modules/auth/decorators/roles.decorator';
+
+/**
+ * کنترلر ادمین برای مدیریت دپارتمان‌ها
+ * مسیرهای CRUD را ارائه می‌دهد
+ */
+@ApiBearerAuth()
+@Roles(Role.MANAGER)
+// api.hrsystem.ir/manager
+@Controller('manager/departments')
+export class DepartmentsManagerController {
+  constructor(private readonly departmentsService: DepartmentsManagerService) {}
+
+  /** POST /admin/departments */
+  @Post()
+  async create(@Body() dto: CreateDepartmentDto) {
+    return await this.departmentsService.create(dto);
+  }
+
+  /** GET /admin/departments */
+  @Get()
+  async findAll() {
+    return await this.departmentsService.findAll();
+  }
+
+  /** GET /admin/departments/:id */
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.departmentsService.findOne(id);
+  }
+
+  /** PATCH /admin/departments/:id */
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateDepartmentDto,
+  ){
+    return await this.departmentsService.update(id, dto);
+  }
+
+  /** DELETE /admin/departments/:id */
+  @Delete(':id')
+  async remove(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<{ success: boolean }> {
+    await this.departmentsService.remove(id);
+    return { success: true };
+  }
+}
