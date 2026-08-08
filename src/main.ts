8ba7a653726e773fa588064bf8e0f8f7e3f7ca0e
shyn <shynaz87@gmail.com>
2026-08-07 22:24:03 -0700

    chore: setup and configure Prisma ORM

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
   const configService = app.get(ConfigService);
   const appName = configService.get('APP_NAME');
   const port = configService.get('PORT', 3001);
+  
   await app.listen(port);
   console.log(`Application ${appName} is running on port : ${port}`);
 }
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,33 @@
+// src/prisma/prisma.service.ts
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import 'dotenv/config';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '../../generated/prisma/client.js';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    // ایجاد آداپتور MariaDB
+    const adapter = new PrismaMariaDb({
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      connectionLimit: 5,
+    });
+
+    // ارسال آداپتور به سازنده کلاس پدر (PrismaClient)
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
