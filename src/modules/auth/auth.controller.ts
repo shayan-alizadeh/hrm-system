25daf149f0ab11826f1e13179faebbbe40caeac5
shyn <shynaz87@gmail.com>
2026-08-11 05:26:47 -0700

    feat : complete refresh method in authcontroller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UnauthorizedExceptiongit } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
@@ -26,6 +26,19 @@ export class AuthController {
     return {
       accessToken: tokens.accessToken,
       refreshToken: tokens.refreshToken,
+    };
+  }
+
+  @Post('refresh')
+  async refresh(@Body() dto: RefreshTokenDto) {
+    if (!dto.refreshToken)
+      throw new UnauthorizedException('Refresh token missing!');
+
+    const tokens = await this.authService.refreshToken(dto.refreshToken);
+
+    return {
+      accessToken: tokens?.newAccessToken,
+      refreshToken: tokens?.newRefreshToken,
     };
   }
 }
