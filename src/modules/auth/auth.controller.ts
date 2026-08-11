0bc17ed76ad91ab1da06ed44163680377a913a51
shyn <shynaz87@gmail.com>
2026-08-11 05:28:54 -0700

    feat : complete refresh-token.dto.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UnauthorizedExceptiongit } from '@nestjs/common';
+import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
