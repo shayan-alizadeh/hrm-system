12ec07680730b6732787d8b8a0b2cb8b63a83ee3
shyn <shynaz87@gmail.com>
2026-08-11 05:13:52 -0700

    feat : complete login method in auth.controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,7 @@
-import { Controller,Post,Body } from "@nestjs/common";
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
-
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +15,17 @@ export class AuthController {
       dto.role,
     );
     return user;
+  }
+
+  @Post('login')
+  async login(@Body() dto: LoginDto) {
+    const user = await this.authService.validateUser(dto.mobile, dto.password);
+
+    const tokens = await this.authService.login(user);
+
+    return {
+      accessToken: tokens.accessToken,
+      refreshToken: tokens.refreshToken,
+    };
   }
 }
