5c608a7183ebf847f2ae9c830f7d96e8a4ed9c0f
shyn <shynaz87@gmail.com>
2026-08-12 12:23:11 -0700

    fix : type error

--- src/modules/auth/guards/roles.guard.ts
+++ src/modules/auth/guards/roles.guard.ts
@@ -6,17 +6,17 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from '../decorators/roles.decorator';
-import { Role } from '../../../shared/enums/user-role.enum';
+import { roleType } from 'generated/prisma/enums';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext) {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const requiredRoles = this.reflector.getAllAndOverride<roleType[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
     if (!requiredRoles || requiredRoles.length === 0) return true;
 
     const { user } = context.switchToHttp().getRequest();
