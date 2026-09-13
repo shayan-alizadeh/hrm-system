1d805f05fbca2ea9f812d87de8b55ff5b9ae4929
shyn <shynaz87@gmail.com>
2026-09-13 13:45:48 +0330

    fix : prisma import

--- src/modules/attendances/services/attendance-employee.service.ts
+++ src/modules/attendances/services/attendance-employee.service.ts
@@ -3,7 +3,7 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service.js';
+import { PrismaService } from '../../../prisma/prisma.service.js';
 import { FilterAttendanceDto } from '../dto/filter-attendance.dto.js';
 
 @Injectable()
